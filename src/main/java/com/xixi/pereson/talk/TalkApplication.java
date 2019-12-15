5bdce695ec1e6038d87ad81347b85509f5a5076b
jaxixi <1934106610@qq.com>
2019-12-15 10:24:27 +0800

    wrote a readme file

---
+++ src/main/java/com/xixi/pereson/talk/TalkApplication.java
@@ -0,0 +1,13 @@
+package com.xixi.pereson.talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkApplication.class, args);
+	}
+
+}
